3fb7c45aa099388b343fd420e700f8cc0fece4ce
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-16 17:07:22 +0100

    Update setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
 const App = () => (
-  <div className="App">
+  <>
     <h1>Hello</h1>
-  </div>
+  </>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import store from 'app/store';
 import App from './App';
 
 const container = document.getElementById('root');
@@ -12,6 +12,5 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
